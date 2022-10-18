81e7a14bcccf654f7acf0a0c4dbc355e7b0d09e9
Ben Zech <72528903+TechZech@users.noreply.github.com>
2022-10-18 19:18:02 -0400

    Typos

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import sklearn
-print(sklearn.__version__) # checks to see if you guys have scikit-learn version. 
-#Should print out 0.22 or higher, like 1.somethign
+
+print(sklearn.__version__)  # checks to see if you guys have scikit-learn version.
+# Should print out 0.22 or higher, like 1.something
 
 if __name__ == "__main__":
     house = input("Enter a house name: ")
     print(house, "is worth $100,000")
-
-    
