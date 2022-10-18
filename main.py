7d9cc2319b0683bfa4d7e14b4877d4455fe955d9
William Steven Matiz <william.s.matiz@gmail.com>
2022-10-18 19:10:59 -0400

    checking if sklern is set up

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import sklearn
+print(sklearn.__version__) # checks to see if you guys have scikit-learn version. 
+#Should print out 0.22 or higher, like 1.somethign
+
 if __name__ == "__main__":
     house = input("Enter a house name: ")
     print(house, "is worth $100,000")
+
+    
