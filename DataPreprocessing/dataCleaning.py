f7f2bbce3c86764a22fb2d117ce8ea51edf3dc76
Ben Zech <72528903+TechZech@users.noreply.github.com>
2022-10-18 20:23:22 -0400

    removed non-dp stuff from dp folder

--- DataPreprocessing/dataCleaning.py
+++ DataPreprocessing/dataCleaning.py
@@ -1,18 +1,8 @@
-import inline as inline
-import matplotlib
-
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-
 import warnings
 
 warnings.filterwarnings('ignore')
 pd.options.display.max_columns = None
-
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import StratifiedKFold
-from sklearn.feature_selection import RFECV
 
 if __name__ == "__main__":
     # set data = the csv file
