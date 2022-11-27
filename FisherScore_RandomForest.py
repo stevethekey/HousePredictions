c04387244d7f1e64a724ae1a98efe26288510f24
gmmourad <gabrielmmourad@gmail.com>
2022-11-27 11:50:16 -0500

    Error with implementation of train_test_split - perform feature engineering AFTER train_test_split not before

--- FisherScore_RandomForest.py
+++ FisherScore_RandomForest.py
@@ -14,6 +14,14 @@ warnings.filterwarnings('ignore')
 pd.options.display.max_columns = None
 
 if __name__ == "__main__":
+    data = pd.read_csv("train_20.csv")
+    X = pd.read_csv("test_20.csv")
+    y = data['SalePrice']
+    my_model = RandomForestRegressor(n_estimators = 300, max_depth = 6)
+    my_model.fit(X, y)
+    y_pred = my_model.predict(X)
+    print (metrics.r2_score(y, y_pred))
+    '''
     for i in range (0,4,1):
         if (i ==0):
             X = pd.read_csv('cleaned.csv')
@@ -24,16 +32,22 @@ if __name__ == "__main__":
         elif (i ==3):
             X = pd.read_csv('LowerQuartile_and_above.csv')
         Y = X['SalePrice']
+        print (Y)
+        
         X_to_use = X.to_numpy()
         Y_to_use = Y.to_numpy()
-        my_model = RandomForestRegressor(n_estimators = 100, random_state=42, max_depth = 6)
-        X_train, X_test, y_train, y_test = train_test_split(X_to_use, Y_to_use, test_size = 0.33, random_state=4)
+        my_model = RandomForestRegressor(n_estimators = 100, random_state=4, max_depth = 6)
+        X_train, X_test, y_train, y_test = train_test_split(X_to_use, Y_to_use, test_size = 0.2, random_state=4)
         my_model.fit(X_train, y_train)
+        print(my_model.score(X_test, y_test))
         y_pred = my_model.predict(X_test)
+        print (y_test)
+        print (y_train)
         my_model_r2 = metrics.r2_score(y_test, y_pred)
-        root_mean_squared_error = (metrics.mean_squared_error(y_test, y_pred, squared=False))
+        root_mean_squared_error = (math.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+        normalized_root_mean_squared_error = root_mean_squared_error/(y_test.max()-y_test.min())
         print ("R Squared", my_model_r2)
-        print ("Root Mean Squared Error", root_mean_squared_error)
+        print ("Root Mean Squared Error", normalized_root_mean_squared_error)
         plt.scatter(y_pred, y_test)
         plt.xlabel('Predicted Value')
         plt.ylabel('Actual Value')
@@ -49,5 +63,5 @@ if __name__ == "__main__":
             f.savefig('Medianandabovefeat.png', dpi = 600)
         elif (i ==3):
             f.savefig('top75pctfeat.png', dpi = 600)
-
+        '''
 
