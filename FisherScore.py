86dc22204556e8e26dfe6186acd64e02b09bddcc
gmmourad <gabrielmmourad@gmail.com>
2022-11-26 20:40:02 -0500

    Gets top 25%, median, top 75%

--- FisherScore.py
+++ FisherScore.py
@@ -7,28 +7,59 @@ import warnings
 
 warnings.filterwarnings('ignore')
 pd.options.display.max_columns = None
+  
+'''
+This file does the following 
+1) Get The fisher Score
+2) Make Plots and export them as images for the 
+    * Features with a fisherscore greater than or equal to the median
+    * Features with a fisherscore in the top 25% or above
+    * Features with a fisherscore in the top 75% or above 
+3) Once we have those plots in a seperate file dataframes will be greated
+from the median, top 25 and top 75
+'''
 
-#using the cleaned data 
-#first we must split into a training dataset and a test dataset 
-#then we will extract the top 20 features 
-#then train the random forest   
 if __name__ == "__main__":
-
     train = pd.read_csv('cleaned.csv')
     test = train
     test.drop(['SalePrice'], axis=1, inplace=True)
-
-    # open the cleaned data
     data = pd.read_csv('cleaned.csv')
     array = data.values
     X= array[:, 0:(len(data.columns)-1)]
     Y = array[:,(len(data.columns)-1)]
-
-    #gets the fisher score for each important feature
     score = fisher_score.fisher_score(X, Y)
-
     feat_importances = pd.Series(score, data.columns[0:len(data.columns)-1])
-    df = feat_importances[score].astype(float).nlargest(20)
+    median = feat_importances.median()
+    twenty_five = feat_importances.quantile(0.25)
+    seventy_five = feat_importances.quantile(0.75)
+    median_count =0
+    twenty_five_count =0
+    seventy_five_count =0
+    for i in score:
+        if (score[i] >= median):
+            median_count+=1
+        if (score[i] >= twenty_five):
+            twenty_five_count+=1
+        if (score[i] >= seventy_five):
+            seventy_five_count+=1
+    df_median = feat_importances[score].astype(float).nlargest(median_count)
+    df_twentyfive = feat_importances[score].astype(float).nlargest(twenty_five_count)
+    df_seventyfive = feat_importances[score].astype(float).nlargest(seventy_five_count)
+    df_median.plot(kind = 'barh', color = 'teal')
+    f = plt.gcf()
+    f.set_size_inches(10, 10)
+    f.savefig('Features_ge_median.png', dpi=600)
+    df_seventyfive.plot(kind = 'barh', color='teal')
+    f = plt.gcf()
+    f.set_size_inches(10,10)
+    f.savefig('Features_top75.png')
+    df_twentyfive.plot(kind = 'barh', color = 'teal')
+    f=plt.gcf()
+    f.set_size_inches(10,10)
+    f.savefig('Features_top25.png')
+    '''  
+    This is old code that will help me for now eventually to be deleted  
+
     dftop20_train = data[['Condition 1', 'Lot Config', 'Bsmt Full Bath', 'MS SubClass', 'Paved Drive', 'Wood Deck SF',
     'Overall Cond', 'Yr Sold', 'Bedroom AbvGr', 'Half Bath', 'Garage Qual', 'Sale Condition', 'Full Bath', 'BsmtFin Type 1', 'Functional', 
     'Lot Area', 'Mas Vnr Area', 'Total Bsmt SF', 'Mas Vnr Type', 'Screen Porch', 'SalePrice']].copy()
@@ -43,7 +74,8 @@ if __name__ == "__main__":
     f = plt.gcf()
     f.set_size_inches(10, 10)
     f.savefig('top20_features_fisher.png', dpi=600)
-    
+
+    '''
     
   
 
