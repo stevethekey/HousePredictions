f29420776c1e4c0809c57fd14d37a4ee153a3914
gmmourad <gabrielmmourad@gmail.com>
2022-11-28 01:58:42 -0500

    UNF has visuals

--- UNF.py
+++ UNF.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.feature_selection import f_regression
 from sklearn.model_selection import train_test_split
 
@@ -9,15 +10,25 @@ if __name__ == "__main__":
     X = data.copy()
     del X['SalePrice']
     Y = data['SalePrice']
-    #f_val, p_val = f_regression(X_train,y_train)
     X_train, X_test, y_train, y_test = train_test_split (X.to_numpy(), Y.to_numpy(), test_size = 0.33, random_state =42)
-    ####this code was obtained from 
+    ####lines 17-20 were obtained from 
     # https://towardsdatascience.com/application-of-feature-selection-techniques-in-a-regression-problem-4278e2efd503
     f_val, p_val = f_regression(X_train,y_train)
-    # creating a dictionary from the arrays
-    feature_dict={'features':X_train.columns.tolist(),
+    feature_dict={'features':X.columns.tolist(),
               'f_score':f_val.tolist()}
-    # creating a sorted dataframe from the dictionary
-    feature_df = pd.DataFrame(feature_dict).sort_values(by='f_scores', ascending=False).reset_index(drop=True)
-    # printing 25 features with the highest scores
-    feature_df.iloc[:25,:]['columns'].tolist()
+    feature_df = pd.DataFrame(feature_dict).sort_values(by='f_score', ascending=False).reset_index(drop=True)
+    ##table to be used in reporting stage
+    df_top20 = feature_df.iloc[0:20]
+    df_top20 = df_top20.reset_index(drop=True)
+    df_top20.reset_index(drop=True, inplace=True)
+    print (df_top20)
+    df_top20.plot(kind = 'barh', color='teal')
+    f = plt.gcf()
+    f.set_size_inches(10, 10)
+    f.savefig('Top20Features.png', dpi=600)
+    feature_df.to_csv("UNF_selection.csv")
+    fig = plt.figure()
+    boxplot = feature_df.boxplot(column = 'f_score')
+    fig.savefig("BoxPlotforF_score.png")
+    
+    ###need to generate visuals for Univariate feature selection 
